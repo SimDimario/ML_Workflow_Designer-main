610ddfb1d71f7230a196ae6ec794a6a368ee7cb1
SimDimario <simonedimario84@gmail.com>
2026-01-28 12:32:32 +0100

    second commit

--- chaM3Leon-be/src/apps/googleDjango/adapters.py
+++
@@ -1,7 +0,0 @@
-# src/apps/googleDjango/adapters.py
-from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-
-class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
-    def get_login_redirect_url(self, request):
-        # Qui indichi la tua view Django che genera JWT
-        return "/auth/social/callback/"
